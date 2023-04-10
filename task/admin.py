648fd29240305249dade42d38efce08961e5331d
zeroylalus <armopc8@gmail.com>
2023-04-10 17:40:59 -0600

    commit

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from task import models
+
+class Taskadmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
 
 # Register your models here.
+admin.site.register(models.Task, Taskadmin)
--- task/views.py
+++ task/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 
 
@@ -18,7 +18,28 @@ def signout(request):
     return redirect('home')
 
 def signin(request):
-    return render(request, 'signin.html')
+
+    if request.method == 'GET':
+        return render(request, 'signin.html', {'form':AuthenticationForm})
+    
+    else:
+
+        user = authenticate(
+            request, username=request.POST['username'],
+            password=request.POST['password'],
+        )
+        if user == None:
+            return render(request, 'signin.html', {
+                'form':AuthenticationForm,
+                'error': 'Username or password is incorrect'
+            })
+        else:
+            login(request, user)
+            return render(request, 'tasks.html', {
+                'form':AuthenticationForm,
+                'signin':'Ya estas loggeado',
+            })
+    
 
 def signup(request):
 
@@ -35,7 +56,7 @@ def signup(request):
                 user.save()
                 login(request, user)
                 return render(request, 'tasks.html', {
-                    'creado' : 'User created successfully'
+                    'signup' : 'User created successfully'
                 })
             except IntegrityError:
                 return render(request, 'signup.html', {
